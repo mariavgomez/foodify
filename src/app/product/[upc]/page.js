e8ef54f41b706f26d546d1ed72e7cf9c7e8e5607
mariavgomez <mvicoria_g23@hotmail.com>
2023-08-28 12:22:01 -0400

    creating the product page

--- src/app/product/[upc]/page.js
+++ src/app/product/[upc]/page.js
@@ -1,7 +1,121 @@
 "use client";
 import { useParams } from "next/navigation";
+// export default function Product() {
+//   const params = useParams();
+//   console.log(params);
+//   return <div>{params.upc}</div>;
+// }
+
+// import { ChevronRightIcon } from "@heroicons/react/20/solid";
+
+const people = [
+  {
+    name: "Leslie Alexander",
+    email: "leslie.alexander@example.com",
+    role: "Co-Founder / CEO",
+    imageUrl:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    href: "#",
+    lastSeen: "3h ago",
+    lastSeenDateTime: "2023-01-23T13:23Z",
+  },
+  {
+    name: "Michael Foster",
+    email: "michael.foster@example.com",
+    role: "Co-Founder / CTO",
+    imageUrl:
+      "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    href: "#",
+    lastSeen: "3h ago",
+    lastSeenDateTime: "2023-01-23T13:23Z",
+  },
+];
+
 export default function Product() {
   const params = useParams();
   console.log(params);
-  return <div>{params.upc}</div>;
+
+  //   function fetchProductInfo(upc) {
+  //     fetch(`/api/product/[upc]`)
+  //       .then((response) => {
+  //         if (!response.ok) {
+  //           throw new Error("Network response was not ok");
+  //         }
+  //         return response.json();
+  //       })
+  //       .then((data) => {
+  //         setProductInfo(data);
+  //         const productIngredients = data.product.ingredients_text;
+  //         setIngredients(productIngredients.split(","));
+  //       })
+  //       .catch((error) => {
+  //         console.error("Error fetching product data:", error);
+  //       });
+  //   }
+  //   fetchProductInfo(data.codeResult.code);
+
+  return (
+    <>
+      <div>{params.upc}</div>
+      <ul
+        role="list"
+        className="divide-y divide-gray-100 overflow-hidden bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl"
+      >
+        {people.map((person) => (
+          <li
+            key={person.email}
+            className="relative flex justify-between gap-x-6 px-4 py-5 hover:bg-gray-50 sm:px-6"
+          >
+            <div className="flex min-w-0 gap-x-4">
+              <img
+                className="h-12 w-12 flex-none rounded-full bg-gray-50"
+                src={person.imageUrl}
+                alt=""
+              />
+              <div className="min-w-0 flex-auto">
+                <p className="text-sm font-semibold leading-6 text-gray-900">
+                  <a href={person.href}>
+                    <span className="absolute inset-x-0 -top-px bottom-0" />
+                    {person.name}
+                  </a>
+                </p>
+                <p className="mt-1 flex text-xs leading-5 text-gray-500">
+                  <a
+                    href={`mailto:${person.email}`}
+                    className="relative truncate hover:underline"
+                  >
+                    {person.email}
+                  </a>
+                </p>
+              </div>
+            </div>
+            <div className="flex shrink-0 items-center gap-x-4">
+              <div className="hidden sm:flex sm:flex-col sm:items-end">
+                <p className="text-sm leading-6 text-gray-900">{person.role}</p>
+                {person.lastSeen ? (
+                  <p className="mt-1 text-xs leading-5 text-gray-500">
+                    Last seen{" "}
+                    <time dateTime={person.lastSeenDateTime}>
+                      {person.lastSeen}
+                    </time>
+                  </p>
+                ) : (
+                  <div className="mt-1 flex items-center gap-x-1.5">
+                    <div className="flex-none rounded-full bg-emerald-500/20 p-1">
+                      <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
+                    </div>
+                    <p className="text-xs leading-5 text-gray-500">Online</p>
+                  </div>
+                )}
+              </div>
+              {/* <ChevronRightIcon
+                className="h-5 w-5 flex-none text-gray-400"
+                aria-hidden="true"
+              /> */}
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
